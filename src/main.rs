4a344d1cc648fcf30051689fefb2c456caa18338
1x2kb <1x2kb@protonmail.com>
2023-06-15 11:00:31 -0500

    Added main

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,7 @@
 mod vehicle;
 
 use aws_lambda_events::event::sns::SnsEvent;
-use base64::{engine::general_purpose, Engine};
 use lambda_runtime::{run, service_fn, Error, LambdaEvent};
-use vehicle::Vehicle;
 
 /// This is the main body for the function.
 /// Write your code inside it.
@@ -11,21 +9,10 @@ use vehicle::Vehicle;
 /// - https://github.com/awslabs/aws-lambda-rust-runtime/tree/main/examples
 /// - https://github.com/aws-samples/serverless-rust-demo/
 async fn function_handler(event: LambdaEvent<SnsEvent>) -> Result<(), Error> {
-    println!("Raw event: \n{:#?}", &event);
     let (sns_event, _context) = event.into_parts();
 
-    let parsed_vehicles: Vec<Vehicle> = sns_event
-        .records
-        .into_iter()
-        .map(|base_64_vehicle| general_purpose::STANDARD.decode(base_64_vehicle.sns.message))
-        .flatten()
-        .map(String::from_utf8)
-        .flatten()
-        .map(|message_data| serde_json::from_str::<Vehicle>(&message_data))
-        .flatten()
-        .collect();
+    lib::execute(sns_event.records).await.unwrap();
 
-    println!("{:#?}", parsed_vehicles);
     Ok(())
 }
 
@@ -41,3 +28,12 @@ async fn main() -> Result<(), Error> {
 
     run(service_fn(function_handler)).await
 }
+
+/*
+"postgres://{}:{}@{}:{}/{}",
+            self.get_username(),
+            self.get_password(),
+            self.get_host(),
+            self.get_port(),
+            self.get_database_name()
+ */
